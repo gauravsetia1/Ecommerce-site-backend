8c6087689b05b6e391637c488d9973cb88e2fb25
Gaurav Setia <gauravsetia1999@gmail.com>
2019-10-03 21:33:20 +0530

    filter created

--- src/main/java/com/casestudy/ecart/controller/ItemsController.java
+++ src/main/java/com/casestudy/ecart/controller/ItemsController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -33,5 +34,26 @@ public class ItemsController {
     public Items getDetailsById(@PathVariable(value="id")Integer itemId)
     {
         return itemsRegister.findById(itemId).orElseThrow(()-> new ResourceNotFoundException("Details","Id",itemId));
+    }
+
+    @GetMapping("/category/{type}")
+    public List<Items> getCategory(@PathVariable(value="type")String categoryType)
+    {
+        return itemsRegister.findAllByCategory(categoryType);
+    }
+
+    @GetMapping("/{cat}/{price1}/{price2}")
+    public List<Items> getCategoryAndPrice(@PathVariable(value = "cat") String category,
+                                           @PathVariable(value = "price1") Double price1,
+                                           @PathVariable(value = "price2") Double price2)
+    {
+        return itemsRegister.findAllByCategoryAndUnitPriceBetween(category,price1,price2);
+    }
+
+    @GetMapping("/{price1}/{price2}")
+    public List<Items> getPrice(@PathVariable(value = "price1") Double price1,
+                                @PathVariable(value = "price2") Double price2)
+    {
+        return itemsRegister.findAllByUnitPriceBetween(price1, price2);
     }
 }
--- src/main/java/com/casestudy/ecart/repository/ItemsRegister.java
+++ src/main/java/com/casestudy/ecart/repository/ItemsRegister.java
@@ -4,7 +4,14 @@ import com.casestudy.ecart.model.Items;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemsRegister extends JpaRepository<Items, Integer> {
 
+    public List <Items> findAllByCategory(String var1);
+
+    public List<Items> findAllByCategoryAndUnitPriceBetween(String category, Double p1, Double p2);
+
+    public List<Items> findAllByUnitPriceBetween(Double p1, Double p2);
 }
